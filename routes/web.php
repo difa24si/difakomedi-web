eb59405e35fa720de404162bb8f89b6dc1f4df8d
difa24si <dif24si@mahasiswa.pcr.ac.id>
2025-10-06 10:03:16 +0700

    berhasil

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\DashboardController;
 
 
 
@@ -37,3 +38,4 @@ Route::get('/pegawai',[PegawaiController::class,'index']);
 Route::post('question/store', [QuestionController::class, 'store'])
 		->name('question.store');
 
+Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
