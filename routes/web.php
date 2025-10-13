8a32018583554e945f785a67ecd4ea70d7437440
difa24si <dif24si@mahasiswa.pcr.ac.id>
2025-10-13 07:47:21 +0700

    penerapa migren tabel pelanggan

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\PelagganController;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\DashboardController;
 
@@ -39,3 +40,4 @@ Route::post('question/store', [QuestionController::class, 'store'])
 		->name('question.store');
 
 Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::resource('pelanggan', PelagganController::class);
